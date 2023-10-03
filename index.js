27fb8db8c203a405743231d82666fe9e0a214547
cristina <cerodriguez017@gmail.com>
2023-10-03 13:39:36 -0700

    respaced

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const vanilla = {Name:"Vanilla", Total:"3"}
 const strawberry = {Name:"Strawberry", Total:"1"}
 const coffee = {Name:"Coffee", Total:"2"}
-
 console.table([vanilla, strawberry, coffee]);
 
 var flavor = prompt("Yogurt flavor you want?");
