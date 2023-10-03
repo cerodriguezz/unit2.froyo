a13aac20d9592cb3d85ee31a742ddbd99e291884
cristina <cerodriguez017@gmail.com>
2023-10-03 13:37:01 -0700

    added my user input table

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const strawberry = {Name:"Strawberry", Total:"1"}
 const coffee = {Name:"Coffee", Total:"2"}
 
 console.table([vanilla, strawberry, coffee]);
+
+var flavor = prompt("Yogurt flavor you want?");
+var num = prompt("How many would you want to order?");
+var flavor2 = prompt("Would you want any other flavor?");
+var num2 = prompt("How many would you want to order?");
+print ("flavor" + flavor);
+print ("total" + num );
+console.table ([flavor, num, flavor2, num2]);
